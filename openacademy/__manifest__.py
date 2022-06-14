b952a0faa2bdae6a439c6efecd085f812c9822d0
JuanBenavente <jbenavente@vauxoo.com>
2022-06-14 14:17:44 -0500

    [REF] openacademy: Added unit tests and fixed lints

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -2,16 +2,14 @@
     'name': "openacademy",
     'summary': """
         This module was created to manage an academy.""",
-    'description': """
-        Courses can be created and users can book sessions.
-    """,
-    'author': "My Company",
+    'author': "Vauxoo",
     'website': "http://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    "license": "LGPL-3",
+    'version': '13.0.1.0.0',
     # any module necessary for this one to work correctly
     'depends': ['base', 'board'],
     # always loaded
--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -1,6 +1,5 @@
 from odoo import models, fields, api, exceptions, _
 from datetime import timedelta
-import time
 
 
 class Session(models.Model):
@@ -13,65 +12,65 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one(
-            'res.partner',
-            domain=['|', ('instructor', '=', True),
-            ('category_id.name', 'ilike', 'Teacher')])
+        'res.partner',
+        domain=['|', ('instructor', '=', True),
+        ('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one(
-            'openacademy.course',
-            ondelete='cascade',
-            required=True)
+        'openacademy.course',
+        ondelete='cascade',
+        required=True)
     attendee_ids = fields.Many2many('res.partner')
-    taken_seats = fields.Float(compute='_taken_seats')
+    taken_seats = fields.Float(compute='_compute_taken_seats')
     active = fields.Boolean(default=True)
     end_date = fields.Date(
-            compute='_get_end_date',
-            inverse='_set_end_date', store=True)
-    attendees_count =  fields.Integer(compute='_get_attendees_count', store=True)
+        compute='_compute_end_date',
+        inverse='_inverse_end_date', store=True)
+    attendees_count = fields.Integer(compute='_compute_attendees_count', store=True)
     color = fields.Integer()
 
     @api.depends('attendee_ids')
-    def _get_attendees_count(self):
+    def _compute_attendees_count(self):
         for record in self:
             record.attendees_count = len(record.attendee_ids)
 
     @api.depends('start_date', 'duration')
-    def _get_end_date(self):
+    def _compute_end_date(self):
         for record in self.filtered('start_date'):
             start_date = fields.Datetime.from_string(record.start_date)
             record.end_date = start_date + timedelta(days=record.duration, seconds=-1)
 
-    def _set_end_date(self):
+    def _inverse_end_date(self):
         for record in self.filtered('start_date'):
             start_date = fields.Datetime.from_string(record.start_date)
             end_date = fields.Datetime.from_string(record.end_date)
             record.duration = (end_date - start_date).days + 1
 
     @api.depends('seats', 'attendee_ids')
-    def _taken_seats(self):
-        for r in self:
-            if not r.seats:
-                r.taken_seats = 0.0
+    def _compute_taken_seats(self):
+        for r_ in self:
+            if not r_.seats:
+                r_.taken_seats = 0.0
             else:
-                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+                r_.taken_seats = 100.0 * len(r_.attendee_ids) / r_.seats
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.seats < 0:
             self.active = False
             return {
-                    'warning': {
-                        'title': _("Incorrect 'seats' value"),
-                        'message': _("The number of available seats may not be negative"),
-                        }
+                'warning': {
+                    'title': _("Incorrect 'seats' value"),
+                    'message': _("The number of available seats may not be negative"),
                     }
+                }
         if self.seats < len(self.attendee_ids):
             self.active = False
             return {
-                    'warning': {
-                        'title': _("Too many attendees"),
-                        'message': _("Increase seats or remove excess attendees"),
-                        }
+                'warning': {
+                    'title': _("Too many attendees"),
+                    'message': _("Increase seats or remove excess attendees"),
                     }
+                }
         self.active = True
 
     @api.constrains('instructor_id', 'attendee_ids')
@@ -79,4 +78,4 @@ class Session(models.Model):
         for record in self.filtered('instructor_id'):
             if record.instructor_id in record.attendee_ids:
                 raise exceptions.ValidationError(
-                        _("A session's instructor can't be an attendee"))
+                    _("A session's instructor can't be an attendee"))
